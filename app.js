147977321fc6b90204cc4f8651fdda947849cf84
syedwaleedali20 <syedwaleed887@gamil.com>
2023-09-18 16:26:09 -0700

    now ready to commit

---
+++ app.js
@@ -0,0 +1,118 @@
+//  var arr=["Nokia","Apple","Samsung","Oppo","Redmi","Tecno"];
+
+//  document.write(
+//    "<select>" +
+//      "<option>" +
+//      arr[0] +
+//      "</option>" +
+//      "<option>" +
+//      arr[2] +
+//      "</option>" +
+//      "<option>" +
+//      arr[1] +
+//      "</option>" +
+//      "<option>" +
+//      arr[3] +
+//      "</option>" +
+//      "<option>" +
+//      arr[4] +
+//      "</option>" +
+//      "<option>" +
+//      arr[5] +
+//      "</option>" +
+//      "<select>"
+//  );\
+
+
+// var animals =["Dog","Cat","Lion","Cheetah","Snake","Elephant"];
+
+// var userinput = prompt("Enter value at the last of Array");
+
+// var userinput1 = prompt("Enter value at the last of Array");
+// var userinput2 = prompt("Enter value at the last of Array");
+// var userinput3 = prompt("Enter value at the last of Array");
+// var userinput4 = prompt("Enter value at the last of Array");
+// var userinput5 = prompt("Enter value at the last of Array");
+
+// animals.push(userinput,userinput1,userinput2,userinput3,userinput4,userinput5);
+
+// console.log(animals)
+
+
+
+//  var animals = ["Dog", "Cat", "Lion", "Cheetah", "Snake", "Elephant"];
+
+// var targetIndex = prompt("Enter value to target the index of array");
+// var deletedIndex = prompt("Enter how many values to remove");
+// var addingElement = prompt("Enter Elements");
+// var addingElement2 = prompt("Enter Elements");
+// var addingElement3 = prompt("Enter Elements");
+
+// animals.splice(
+//   targetIndex,
+//   deletedIndex,
+//   addingElement,
+//   addingElement2,
+//   addingElement3
+// );
+
+// console.log("Updated Array", animals);
+
+// var animals=["Babar","Shadab","Shaheen","Zaman","Haris Rauf"];
+// var startingvalue = prompt("Enter Starting Value");
+// var endingvalue = prompt("Enter Ending Value");
+
+// animals.slice(startingvalue,endingvalue);
+
+// console.log("Updated Array:",animals.slice(startingvalue,endingvalue));
+
+
+// for (var i=0; i <=10; i = i+2)
+// {
+//     console.log(i);
+// }
+
+
+// var userInput = +prompt("Enter a number to generate a multiplication table");
+
+// var tablelength=+prompt("Enter table Length")
+
+// for (var i=1; i <= tablelength; i++)
+// {
+// document.write(userInput + "x" + i + "=" + userInput * i + "<br>")
+// };
+
+
+
+// var arr = ["abc", 100, true, false, "xyz"];
+
+// for (var i = 0;i < arr.length; i++)
+// {
+//     console.log(arr[i]);
+// }
+
+
+// var fruits = ["Banana","Mango","Apple","Orange","Grapes","PineApple"];
+// for(var i = 0; i <fruits.length; i++){
+//     console.log("Elements of " + fruits[i] + " at " + " " + i)
+// }
+
+
+// Chapter no1 ARRAYS AND LOOP 
+
+// Q3
+
+// for (var i=0; i <= 10; i = i++)
+// {
+//     console.log(i);
+// }
+
+// Q4
+// var userInput = +prompt("Enter a number to generate a multiplication table");
+// var tablelength = +prompt("Enter table length");
+
+// for (var i = 1; i <= tablelength; i++) {
+//     document.write(userInput + "x" + i + "=" + userInput * i + "<br>")
+// }
+
+//Q5
